adf34c8263443fc07f753f205189cb540545a548
dan abbott <dan.abbott@bedegaming.com>
2015-12-18 01:42:00 +0000

    moved api to server - just messing about

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+var employees = {};
+
+var jsonData={
+  APIKey: "37ce2890-aa84-4997-b204-2bab98c954c0",
+  Action: "GetAllEmployeeDetail",
+  IncludeLeavers:"false"
+}
+
+request.post(
+  'https://api.peoplehr.net/Employee',
+  { json: jsonData },
+  function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      employees = body.Result
+    }
+  }
+);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { dave: employees });
 });
 
 module.exports = router;
